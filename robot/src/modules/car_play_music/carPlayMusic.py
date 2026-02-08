b578b3249a14c17d56bf1135003cf2d6a8088f59
AlexanderWedekind <awedwork@gmail.com>
2026-02-08 22:00:50 +0000

    adds menu to stop/start/quit music

--- robot/src/modules/car_play_music/carPlayMusic.py
+++ robot/src/modules/car_play_music/carPlayMusic.py
@@ -1,5 +1,6 @@
 from picarx import Picarx
-from robot_hat import Music
+from robot_hat.robot_hat.music import Music
+import readchar
 
 music = Music()
 
@@ -9,6 +10,25 @@ def playMusic():
     music.music_play('../../../music/423321__dominictreis__the-doofus-sneaks-around.mp3')
     music.sound_play('../../../sounds/car-double-horn.wav')
 
+def playMusicNonStop():
+    music.music_set_volume(25)
+    music.sound_play('../../../sounds/car-engine-start.wav')
+    music.sound_play('../../../sounds/car-double-horn.wav')
+    print('-- press [m] to start/stop music; press [q] to quit menu --')
+    musicOn = False
+    while True:
+        keyStroke = readchar.readkey().lower()
+        if keyStroke == 'm':
+            if musicOn == False:
+                musicOn = True
+            else:
+                musicOn = False
+        elif keyStroke == 'q':
+            break
+        while musicOn == True:
+            music.music_play('../../../music/423321__dominictreis__the-doofus-sneaks-around.mp3')
 
+            
 
+    
 
