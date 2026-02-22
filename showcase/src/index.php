c83b35a4454a76d8d5d439d807f31b14b43e7315
AlexanderWedekind <awedwork@gmail.com>
2026-02-22 16:43:06 +0000

    simple php file to test adding to an azure webapp

---
+++ showcase/src/index.php
@@ -0,0 +1,5 @@
+<?php
+    include_once('php/helloWorld.php')
+?>
+
+<h1> <?php hello_world() ?> </h1>
---
+++ showcase/src/php/helloWorld.php
@@ -0,0 +1,5 @@
+<?php
+    function hello_World() {
+        echo('<h1>Hello World</h1>');
+    }
+?>
