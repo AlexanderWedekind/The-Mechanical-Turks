a812cf14a02b582cab8f2993e0979e1089a8acca
AlexanderWedekind <awedwork@gmail.com>
2026-04-21 10:47:09 +0100

    fixes forgotten file-rename in import statement

--- robot/src/main.py
+++ robot/src/main.py
@@ -1,5 +1,6 @@
 from modules.menus.main_menu import MainMenu
-from modules.hardware_initialisation import initHardware, hardwareCleanup
+from modules.hardware_startup import initHardware
+from modules.hardware_cleanup import hardwareCleanup
 from modules.safe_call_with_except_logging import crashCallLogExc, contCallLogExc
 
 def Main():
