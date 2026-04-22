26245dbd971a0ce4be24256c57e6a0e60c4e38cb
AlexanderWedekind <awedwork@gmail.com>
2026-04-22 15:20:46 +0100

    same

--- robot/src/modules/hardware_startup.py
+++ robot/src/modules/hardware_startup.py
@@ -20,7 +20,7 @@ def initMusic():
     print('- Music initialised...')
 
 def initTTS():
-    robotParts['tts'] = TTS.land('en-US')
+    robotParts['tts'] = TTS.lang('en-US')
     print('- TTS initialised...')
 
 def initMotors():
