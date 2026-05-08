a79a7df88f5cfadae1e40df307ac2bc00ea8769c
AlexanderWedekind <awedwork@gmail.com>
2026-05-08 14:12:34 +0100

    fixes syntax error

--- robot/src/modules/state/robot/road_markings.py
+++ robot/src/modules/state/robot/road_markings.py
@@ -49,7 +49,7 @@ def readRoadMarkings():
             stateChanged = False
             if consistentLineReads() == True:
                 for i in range(3):
-                    if keptLineReads[0][i] != robotState['road_markings'][len(robotState['road_markings'] - 1)][i]:
+                    if keptLineReads[0][i] != robotState['road_markings'][len(robotState['road_markings']) - 1][i]:
                         stateChanged = True
             if stateChanged == True:
                 robotState['road_markings'].append(keptLineReads)
