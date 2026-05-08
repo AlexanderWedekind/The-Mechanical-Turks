6391f4c56b650e31189160f1a65b16a41b7f5c5f
AlexanderWedekind <awedwork@gmail.com>
2026-05-08 13:51:51 +0100

    fixes syntax errors

--- robot/src/modules/actions/detect_junctions.py
+++ robot/src/modules/actions/detect_junctions.py
@@ -7,7 +7,7 @@ def junctionDirection(values):
             finding = 'left'
         elif values[2] == True:
             finding = 'right'
-        elif values[0] == True && values[2] == True:
+        elif values[0] == True & values[2] == True:
             finding = 'crossroads'
         else:
             finding = 'no'
@@ -18,7 +18,7 @@ def junctionDirection(values):
 def junctionDetected(values):
     finding = False
     if values[1] == True:
-        if values[0] == True || values[2] == True:
+        if values[0] == True | values[2] == True:
             finding = True
     return finding
 
@@ -29,13 +29,4 @@ def findJunction():
             if junctionDetected(robotState['road_markings'][length - i]) == True:
                 return junctionDirection(robotState['road_markings'][length - i])
     return 'no'
-
-def passedJunction():
-    junctionPassed = False
-    one = robotState['road_markings'][len(robotState['road_markings'] - 1)]
-    two = robotState['road_markings'][len(robotState['road_markings'] - 2)]
-    three = robotState['road_markings'][len(robotState['road_markings'] - 3)]
-    if one[0] == False && one[1] == False && one[2] == False:
-        if (two[0] == True && two[1] == True && two[2] == False) or (two[0] == False && two[1] == True && two[2] == True):
-
 
